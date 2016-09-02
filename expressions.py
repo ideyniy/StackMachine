ff22da3f35cf34fe6fa571a4e2fb7ca29d073b3c
ideyniy.ii <ideyniy.ii@gmail.com>
2016-09-02 21:00:48 +0200

    init commit

---
+++ expressions.py
@@ -0,0 +1,154 @@
+"""
+a Simple xml parser fed stack machine,
+producing results of operations into xml.
+
+Stack machine implemented, based on
+sources: http://csl.sublevel3.org/post/vm/
+https://github.com/cslarsen/python-simple-vm
+"""
+
+from collections import deque
+from xml.etree.ElementTree import ElementTree, Element, SubElement
+from os import listdir
+from os.path import isfile, join, splitext
+import sys
+
+
+class ConvertUtils(object):
+    @staticmethod
+    def to_int(string, supress_exception=True):
+        value = None
+        try:
+            value = int(float(string))
+        except ValueError as valueError:
+            if not supress_exception:
+                raise valueError
+            else:
+                pass
+        except TypeError as typeError:
+            if not supress_exception:
+                raise typeError
+            else:
+                pass
+        return value
+
+    @staticmethod
+    def to_string(stringToConvert):
+        return  str(stringToConvert)
+
+    @staticmethod
+    def data_to_xml_node(data):
+        elm = Element("expressions")
+        for r_id, result in data:
+            sub = SubElement(elm, "result", id=str(r_id))
+            sub.text = str(result)
+        return elm
+
+
+
+
+class Machine(object):
+    """
+    a Stack Machine interpreting the following syntax:
+    <expressions> : [(<operation>, [(<operand> or None, <expressions>),..], <result_id>),..]
+    """
+
+    def __init__(self):
+        self.data_stack = deque()
+        self.dispatch_map = dict(addition=int.__add__, subtraction=int.__sub__, multiplication=int.__mul__, division=int.__floordiv__)
+
+    def pop(self):
+        return self.data_stack.pop()
+
+    def push(self, value):
+        self.data_stack.append(value)
+
+    def run(self, code):
+        return [(res, self.dispatch((ins, op, None)) or self.pop())
+                for ins, op, res in code]
+
+    def dispatch(self, op):
+        #print op
+        if not op:
+            pass
+        elif isinstance(op, str) and op in self.dispatch_map:
+            self.execute(op)
+        elif isinstance(op, int):
+            self.push(op)
+        elif isinstance(op, tuple) and len(op) == 2:
+            map(self.dispatch, op)
+        elif isinstance(op, tuple) and len(op) == 3:
+            ins, ops, _ = op
+            map(self.dispatch, ops)
+            for _ in range(len(ops)-1):
+                self.dispatch(ins)
+        elif isinstance(op, list):
+            map(self.dispatch, op)
+        else:
+            raise RuntimeError("Unknown operation: '%s'" % op)
+
+    def execute(self, op):
+        last = self.pop()
+        self.push(self.dispatch_map[op](self.pop(), last))
+
+
+class XmlReader(object):
+    """
+    a Parser from XML to Machine interpretable code
+    """
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def parse(self):
+        root = ElementTree().parse(self.file_name)
+        return self._elm_to_code(root)
+
+    def _elm_to_code(self, elm):
+        code = []
+        expressions = elm
+        for operation in expressions:
+            operands = []
+            for operand in operation:
+                constant = ConvertUtils.to_int(operand.text.strip())
+                sub_operation = self._elm_to_code(operand)
+                operands.append((constant, sub_operation))
+            result_id = ConvertUtils.to_int(operation.attrib.get("id"))
+            complex = ConvertUtils.to_string(operation.attrib.get("complex"))
+            code.append((operation.tag, operands, result_id, complex))
+        return code
+
+
+class XmlWriter(object):
+    """
+    a Converter from the Stack Machine output into XML
+    """
+    def __init__(self, file_name, output_dir=None):
+        self.file_name = '%s_result.xml' % file_name[:-4]
+        if output_dir:
+            self.file_name = join(output_dir, self.file_name)
+
+    def write(self, data):
+        tree = ElementTree()
+        tree._setroot(ConvertUtils.data_to_xml_node(data))
+        tree.write(self.file_name)
+
+
+def get_input_files(dir, file_extension, result_postfix):
+    return [f for f in listdir(dir) if (isfile(join(dir, f)) and not splitext(f)[0].endswith(result_postfix) and splitext(f)[1] == file_extension)]
+
+if __name__ == "__main__":
+    print('Usage python.exe expressions.py PathToInputDir PathToOutputDir')
+    _, input_dir, output_dir = sys.argv
+    input_files = get_input_files(input_dir, '.xml', '_result')
+    vm = Machine()
+    for xml_file in input_files:
+        writer = XmlWriter(xml_file, output_dir)
+        code = XmlReader(input_dir+xml_file).parse()
+        print(code)
+        res = vm.run(code)
+        print(res)
+        writer.write(res)
+
+
+
+
---
+++ tests.py
@@ -0,0 +1,146 @@
+import sys
+import unittest
+import expressions as expr
+from xml.etree.ElementTree import ElementTree, Element, SubElement
+
+# Please change this variable to yours!
+unittest_data_dir = 'C:\\Users\\netov\\Documents\\Solution\\unittests-data\\'
+simple_xml = unittest_data_dir+'simplevaluation-test\simple.xml'
+complex_xml = unittest_data_dir + 'complexvaluation-test\complex.xml'
+float_xml = unittest_data_dir+'complexvaluation-test\/float.xml'
+negative_xml = unittest_data_dir+'complexvaluation-test\/negative.xml'
+
+
+class TestConvertUtils(unittest.TestCase):
+
+    def test_to_int_equal(self):
+        self.assertEqual(expr.ConvertUtils.to_int('123'), 123, "Elements should be different")
+
+    def test_to_int_non_equal(self):
+        self.assertNotEqual(expr.ConvertUtils.to_int('321'), 123, "Elements should be different")
+
+    # Calculations with float
+    def test_int_to_float(self):
+        self.assertEquals(expr.ConvertUtils.to_int("2.02"), 2.02)
+
+    # Check complex tag parsed
+    def test_complex_tag_true(self):
+        code = expr.XmlReader(complex_xml).parse()
+        result = 'true' in code[0]
+        self.assertEqual(result,True, "Complex tag missed")
+
+    def test_to_int_expect_value_error_exception(self):
+        with self.assertRaises(ValueError):
+            expr.ConvertUtils.to_int('123as.1', False)
+
+    def test_to_int_expect_type_error_exception(self):
+        with self.assertRaises(TypeError):
+            expr.ConvertUtils.to_int([1,2,3], False)
+
+    def test_data_to_elm_wrong_data(self):
+        with self.assertRaises(TypeError):
+            res = expr.ConvertUtils.data_to_xml_node(None)
+
+    def test_data_to_elm_correct_instance(self):
+        input_data = [(1, 2)]
+        res = expr.ConvertUtils.data_to_xml_node(input_data)
+        self.assertEqual(True, isinstance(res, Element))
+
+    def test_data_to_elm_correct_len(self):
+        input_data = [(1, 2), (3, 4), (5, 6)]
+        res = expr.ConvertUtils.data_to_xml_node(input_data)
+        children = res.getchildren()
+        self.assertEqual(3, len(children))
+
+    def test_data_to_elm_correct_ids(self):
+        input_data = [(1, 2), (3, 4), (5, 6)]
+        res = expr.ConvertUtils.data_to_xml_node(input_data)
+        children = res.getchildren()
+        self.assertEqual('1', children[0].get("id"))
+        self.assertEqual('3', children[1].get("id"))
+        self.assertEqual('5', children[2].get("id"))
+
+
+class TestInputFiles(unittest.TestCase):
+
+    def test_input_files_returns_list(self):
+        global unittest_data_dir
+        files = expr.get_input_files(unittest_data_dir+'filelisting-test', '.xml', '_result')
+        self.assertEqual(True, isinstance(files, list))
+
+    def test_input_files_expected_error(self):
+        global unittest_data_dir
+        with self.assertRaises(WindowsError):
+            expr.get_input_files('UNKNOWN_DIR', '.xml', '_result')
+
+    def test_input_files(self):
+        global unittest_data_dir
+        files = expr.get_input_files(unittest_data_dir+'filelisting-test', '.xml', '_result')
+        self.assertEqual('file1.xml', files[0])
+        self.assertEqual('file2.xml', files[1])
+        self.assertEqual('file3.xml', files[2])
+
+
+class TestValuationSimple(unittest.TestCase):
+
+    def test_simple_valuation(self):
+        code = expr.XmlReader(simple_xml).parse()
+        vm = expr.Machine()
+        res = vm.run(code)
+        self.assertEqual(res[0][0], 1)
+        self.assertEqual(res[0][1], 9)
+        self.assertEqual(res[1][0], 2)
+        self.assertEqual(res[1][1], 1)
+        self.assertEqual(res[2][0], 3)
+        self.assertEqual(res[2][1], 240)
+        self.assertEqual(res[3][0], 4)
+        self.assertEqual(res[3][1], 6)
+
+class TestValuationComplex(unittest.TestCase):
+
+    def test_complex_valuation(self):
+        code = expr.XmlReader(complex_xml).parse()
+        vm = expr.Machine()
+        res = vm.run(code)
+        self.assertEqual(res[0][0], 10)
+        self.assertEqual(res[0][1], 9)
+        self.assertEqual(res[1][0], 11)
+        self.assertEqual(res[1][1], 1)
+        self.assertEqual(res[2][0], 12)
+        self.assertEqual(res[2][1], 240)
+        self.assertEqual(res[3][0], 13)
+        self.assertEqual(res[3][1], 1814400)
+        self.assertEqual(res[4][0], 14)
+        self.assertEqual(res[4][1], 6)
+
+class TestValuationComplexNegative(unittest.TestCase):
+
+    def test_complex_negative_valuation(self):
+        code = expr.XmlReader(negative_xml).parse()
+        vm = expr.Machine()
+        res = vm.run(code)
+        self.assertEqual(res[0][0], 10)
+        self.assertEqual(res[0][1], -3)
+        # self.assertEqual(res[1][0], 11)
+        # self.assertEqual(res[1][1], 1)
+        # self.assertEqual(res[2][0], 12)
+        # self.assertEqual(res[2][1], 240)
+        # self.assertEqual(res[3][0], 13)
+        # self.assertEqual(res[3][1], 1814400)
+        # self.assertEqual(res[4][0], 14)
+        # self.assertEqual(res[4][1], 6)
+
+class TestValuationComplexFloat(unittest.TestCase):
+
+    def test_float_valuation(self):
+        global unittest_data_dir
+        input_file = unittest_data_dir+'complexvaluation-test\/float.xml'
+        code = expr.XmlReader(input_file).parse()
+        vm = expr.Machine()
+        res = vm.run(code)
+        self.assertEqual(res[0][0], 10)
+        self.assertEqual(res[0][1], 10.01)
+
+if __name__ == '__main__':
+    unittest.main()
+
